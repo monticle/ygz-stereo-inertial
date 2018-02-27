df86990665850441050e3643668da4b461d56066
monticle <wang.lei@ilooktech.com>
2018-02-27 18:59:58 +0900

    update for leadsense

---
+++ build.sh
@@ -0,0 +1,104 @@
+bsfile="buildok.log"
+
+echo "Configuring and building Thirdparty/Pangolin ..."
+if [ ! -x "Thirdparty/Pangolin" ]; then
+	cd Thirdparty
+	git clone https://github.com/stevenlovegrove/Pangolin.git
+	if [ $? -ne 0 ];then
+		echo "clone pangolin failed..."
+		exit 1
+	fi
+	cd ..
+fi
+
+cd Thirdparty/Pangolin
+mkdir build
+cd build
+if [ ! -f "$bsfile" ]; then
+	cmake .. -DCMAKE_BUILD_TYPE=Release
+	make -j4
+
+	if [ $? -ne 0 ];then
+		echo "failed..."
+		exit 1
+	fi
+	touch "$bsfile"
+else
+	echo "already build ok. skip.."
+fi
+
+
+
+
+cd ../../..
+echo "Configuring and building Thirdparty/DBoW2 ..."
+
+cd Thirdparty/DBoW2
+mkdir build
+cd build
+if [ ! -f "$bsfile" ]; then
+	cmake .. -DCMAKE_BUILD_TYPE=Release -DOpenCV_DIR=/opt/OpenCV320/share/OpenCV
+	make -j4
+
+	if [ $? -ne 0 ];then
+		echo "failed..."
+		exit 1
+	fi
+	touch "$bsfile"
+else
+	echo "already build ok. skip.."
+fi
+
+
+
+
+cd ../../..
+echo "Configuring and building Thirdparty/g2o ..."
+
+cd Thirdparty/g2o
+mkdir build
+cd build
+if [ ! -f "$bsfile" ]; then
+	sudo apt-get install libblas-dev
+	sudo apt-get install liblapack-dev
+	cmake .. -DCMAKE_BUILD_TYPE=Release
+	make -j4
+
+	if [ $? -ne 0 ];then
+		echo "failed..."
+		exit 1
+	fi
+	touch "$bsfile"
+else
+	echo "already build ok. skip.."
+fi
+
+
+cd ../../..
+echo "Configuring and building Thirdparty/fast ..."
+
+cd Thirdparty/fast
+mkdir build
+cd build
+if [ ! -f "$bsfile" ]; then
+	cmake .. -DCMAKE_BUILD_TYPE=Release -DOpenCV_DIR=/opt/OpenCV320/share/OpenCV
+	make -j4
+
+	if [ $? -ne 0 ];then
+		echo "failed..."
+		exit 1
+	fi
+	touch "$bsfile"
+else
+	echo "already build ok. skip.."
+fi
+
+
+
+cd ../../..
+echo "Configuring and building ygz-stereo-inertial ..."
+sudo apt-get install libgoogle-glog-dev
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DOpenCV_DIR=/opt/OpenCV320/share/OpenCV
+make VERBOSE=1
---
+++ leadsense/leadsense.cpp
@@ -0,0 +1,133 @@
+#include<iostream>
+#include<algorithm>
+#include<fstream>
+#include<iomanip>
+#include<chrono>
+
+#include<opencv2/opencv.hpp>
+
+#include<ygz/System.h>
+
+//Evo SDK header
+#include <evo_global_define.h>//global define
+#include <evo_stereocamera.h>//stereo camera
+#include <evo_mat.h>//evo Mat define
+#include <evo_matconverter.h>//evo Mat converter
+
+using namespace ygz;
+using namespace std;
+using namespace evo;
+using namespace evo::bino;
+
+bool genYaml(StereoParameters param, Resolution_FPS resfps){
+	ofstream fs("leadsense.yaml", ios_base::out);
+	if(!fs.is_open()){
+		cerr << "error to open leadsense.yaml" << endl;
+		return false;	
+	}
+	fs << "%YAML:1.0" << endl << "---" << endl << endl;
+	fs << "PureVisionMode: true" << endl;
+	fs << "UseViewer: true" << endl << endl;
+	fs << "Camera.fx: " << param.leftCam.focal.x << endl;
+	fs << "Camera.fy: " << param.leftCam.focal.y << endl;
+	fs << "Camera.cx: " << param.leftCam.center.x << endl;
+	fs << "Camera.cy: " << param.leftCam.center.y << endl;
+	fs << "Camera.k1: " << 0.0 << endl;
+	fs << "Camera.k2: " << 0.0 << endl;
+	fs << "Camera.p1: " << 0.0 << endl;
+	fs << "Camera.p2: " << 0.0 << endl;
+	fs << "Camera.width: " << resfps.width << endl;
+	fs << "Camera.height: " << resfps.height << endl;
+	// Camera frames per second 
+	fs << "Camera.fps: " << resfps.fps << endl;
+	// stereo baseline[m] times fx
+	fs << "Camera.bf: " << param.T.x / 1000 * param.leftCam.focal.x << endl;
+	
+	fs.close();
+	return true;
+}
+
+int main(int argc, char **argv)
+{
+    cout << endl << "add parameter to change resolution : ./leadsense [800|400(default)]" << endl;
+    google::InitGoogleLogging(argv[0]);
+
+	std::cout << "init evo stereo camera... " << std::endl;
+	StereoCamera camera;
+	bool running = false;
+	int width, height;
+
+	int cnt = 0, limit = 100;
+	double tempt = 0;
+	StereoParameters stereoPara;//stereo parameter
+
+	//open camera
+	RESOLUTION_FPS_MODE res_mode = RESOLUTION_FPS_MODE_SD400_30;
+	if(argc > 1)
+	{
+		int res_n = atoi(argv[2]);
+		if(res_n == 800){
+			res_mode = RESOLUTION_FPS_MODE_HD800_30;
+		}
+	}
+	RESULT_CODE res = camera.open(res_mode);
+	std::cout << "stereo camera open: " << result_code2str(res) << std::endl;
+	//show image size
+	width = camera.getImageSizeFPS().width;
+	height = camera.getImageSizeFPS().height;
+	std::cout << "image width:" << width << ", height:" << height << std::endl;
+
+	if(genYaml(camera.getStereoParameters(), camera.getImageSizeFPS()) == false){
+		return 2;
+	}
+ 	// Create SLAM system. It initializes all system threads and gets ready to process frames.
+    System system("leadsense.yaml");
+    
+	if (res == RESULT_CODE_OK)//open camera successed
+	{
+		//evo Mat
+		evo::Mat<unsigned char> evo_image;
+		//cv Mat
+		cv::Mat cv_image;
+		cv::Mat imLeft, imRight;
+    
+		std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+
+
+		//running flag
+		running = true;
+		//main loop
+		while (running)
+		{
+			// Get frames and launch the computation
+			if (camera.grab(true) == RESULT_CODE_OK)
+			{
+				evo_image = camera.retrieveImage(SIDE_SBS);
+				//Mat convert
+				cv_image = evoMat2cvMat(evo_image);
+				
+				// Read left and right images from file
+				imLeft = cv_image(cv::Rect(0,0,width, height));
+				imRight = cv_image(cv::Rect(width,0,width, height));
+				double tframe = camera.getTargetFrameTimeCode();
+
+				// Pass the images to the SLAM system
+				system.AddStereo(imLeft,imRight,tframe); 
+
+				double ttrack= std::chrono::duration_cast<std::chrono::duration<double> >(std::chrono::steady_clock::now() - t1).count();
+      			t1 = std::chrono::steady_clock::now();
+
+				cnt++;
+				tempt += ttrack;
+				if(cnt == limit){
+					cout << "avg loop time:" << tempt / limit << endl;
+					cnt = 0; 
+					tempt = 0;
+				}
+				//std::this_thread::sleep_for(std::chrono::microseconds(5));
+			}      
+		}
+	}
+    
+    return 0;
+}
