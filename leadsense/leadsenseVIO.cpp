a9c79abf9e13d1222733bc2b7b05884d822924ea
monticle <wang.lei@ilooktech.com>
2018-03-23 17:16:47 +0900

    VIO改为外部开启线程取imu数据

--- leadsense/leadsenseVIO.cpp
+++ leadsense/leadsenseVIO.cpp
@@ -21,9 +21,9 @@ using namespace evo::bino;
 
 bool genYaml(StereoParameters param, Resolution_FPS resfps){
 	ofstream fs("leadsense.yaml", ios_base::out);
-	if(!fs.is_open()){
+	if (!fs.is_open()){
 		cerr << "error to open leadsense.yaml" << endl;
-		return false;	
+		return false;
 	}
 	fs << "%YAML:1.0" << endl << "---" << endl << endl;
 	fs << "PureVisionMode: true" << endl;
@@ -47,13 +47,36 @@ bool genYaml(StereoParameters param, Resolution_FPS resfps){
 	return true;
 }
 
+StereoCamera camera;
+bool running;
+// imu
+std::mutex imu_lock;
+VecIMU vimu;
+evo::imu::IMUData imu_rawdata;
+void retrieveIMUData()
+{
+	while (running){
+		imu_lock.lock();
+
+		float lastgx = imu_rawdata.gyro[0];
+		imu_rawdata = camera.retrieveIMUData();
+		if (lastgx != imu_rawdata.gyro[0])
+		{
+			ygz::IMUData imudata = ygz::IMUData(imu_rawdata.gyro[0], imu_rawdata.gyro[1], imu_rawdata.gyro[2],
+				imu_rawdata.accel[0], imu_rawdata.accel[1], imu_rawdata.accel[2], imu_rawdata.timestamp);
+			vimu.push_back(imudata);
+		}
+
+		imu_lock.unlock();
+		std::this_thread::sleep_for(std::chrono::microseconds(1));
+	}
+
+}
 
 int main(int argc, char **argv) {
 	cout << endl << "add parameter to change resolution : ./leadsense [800|400(default)]" << endl;
-    google::InitGoogleLogging(argv[0]);
+	google::InitGoogleLogging(argv[0]);
 	std::cout << "init evo stereo camera... " << std::endl;
-	StereoCamera camera;
-	bool running = false;
 	int width, height;
 
 	int cnt = 0, limit = 100;
@@ -62,10 +85,10 @@ int main(int argc, char **argv) {
 
 	//open camera
 	RESOLUTION_FPS_MODE res_mode = RESOLUTION_FPS_MODE_SD400_30;
-	if(argc > 1)
+	if (argc > 1)
 	{
 		int res_n = atoi(argv[2]);
-		if(res_n == 800){
+		if (res_n == 800){
 			res_mode = RESOLUTION_FPS_MODE_HD800_30;
 		}
 	}
@@ -77,32 +100,32 @@ int main(int argc, char **argv) {
 	std::cout << "image width:" << width << ", height:" << height << std::endl;
 
 	//check IMU
-	if(camera.isIMUSupported() == false){
+	if (camera.isIMUSupported() == false){
 		std::cout << "IMU not support for this camera." << std::endl;
 		return 2;
 	}
-	if(camera.startRetrieveIMU() != RESULT_CODE::RESULT_CODE_OK){
+	if (camera.startRetrieveIMU() != RESULT_CODE::RESULT_CODE_OK){
 		std::cout << "Open IMU failed." << std::endl;
 		return 3;
 	}
-	
-	if(genYaml(camera.getStereoParameters(), camera.getImageSizeFPS()) == false){
+
+	if (genYaml(camera.getStereoParameters(), camera.getImageSizeFPS()) == false){
 		return 4;
 	}
- 	// Create SLAM system. It initializes all system threads and gets ready to process frames.
-    System system("leadsense.yaml");
+	// Create SLAM system. It initializes all system threads and gets ready to process frames.
+	System system("leadsense.yaml");
 
-    // set TBC
-    Matrix3d Rbc_;
-    Vector3d tbc_;
-    Rbc_ <<
-             1, 0, 0,
-             0, -1, 0,
-             0, 0, -1;
-    tbc_ <<
-             -0.0475, 0.0032, -0.004;
+	// set TBC
+	Matrix3d Rbc_;
+	Vector3d tbc_;
+	Rbc_ <<
+		1, 0, 0,
+		0, -1, 0,
+		0, 0, -1;
+	tbc_ <<
+		-0.0475, 0.0032, -0.004;
 
-    setting::TBC = SE3d(Rbc_, tbc_);
+	setting::TBC = SE3d(Rbc_, tbc_);
 
 	if (res == RESULT_CODE_OK)//open camera successed
 	{
@@ -111,52 +134,51 @@ int main(int argc, char **argv) {
 		//cv Mat
 		cv::Mat cv_image;
 		cv::Mat imLeft, imRight;
-    	// imu
-        VecIMU vimu;
-		evo::imu::IMUData rawdata;
-		std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
 
-		//running flag
 		running = true;
+		VecIMU vimu2;
+		std::thread imu_thread(retrieveIMUData);
+		imu_thread.detach();
+
+		std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
 		//main loop
 		while (running)
 		{
-			float lastgx = rawdata.gyro[0];
-			rawdata = camera.retrieveIMUData();
-			if(lastgx != rawdata.gyro[0]){
-				ygz::IMUData imudata = ygz::IMUData(rawdata.gyro[0],rawdata.gyro[1],rawdata.gyro[2],rawdata.accel[0],rawdata.accel[1],rawdata.accel[2],rawdata.timestamp);
-				vimu.push_back(imudata);
-			}
-			
 			// Get frames and launch the computation
 			if (camera.grab(true) == RESULT_CODE_OK)
 			{
+				imu_lock.lock();
+				std::copy(vimu.begin(), vimu.end(), std::back_inserter(vimu2));
+				vimu.clear();
+				imu_lock.unlock();
 				evo_image = camera.retrieveImage(SIDE_SBS);
+
 				//Mat convert
 				cv_image = evoMat2cvMat(evo_image);
-				
+
 				// Read left and right images from file
-				imLeft = cv_image(cv::Rect(0,0,width, height));
-				imRight = cv_image(cv::Rect(width,0,width, height));
+				imLeft = cv_image(cv::Rect(0, 0, width, height));
+				imRight = cv_image(cv::Rect(width, 0, width, height));
 				double tframe = camera.getTargetFrameTimeCode();
 
 				// Pass the images and imu data to the SLAM system
-				system.AddStereoIMU(imLeft, imRight, tframe, vimu);
-				double ttrack= std::chrono::duration_cast<std::chrono::duration<double> >(std::chrono::steady_clock::now() - t1).count();
-      			t1 = std::chrono::steady_clock::now();
+				system.AddStereoIMU(imLeft, imRight, tframe, vimu2);
+				double ttrack = std::chrono::duration_cast<std::chrono::duration<double> >(std::chrono::steady_clock::now() - t1).count();
+				t1 = std::chrono::steady_clock::now();
 
 				cnt++;
 				tempt += ttrack;
-				if(cnt == limit){
+				if (cnt == limit){
 					cout << "avg loop time:" << tempt / limit << endl;
-					cnt = 0; 
+					cnt = 0;
 					tempt = 0;
 				}
-				vimu.clear();
-				//std::this_thread::sleep_for(std::chrono::microseconds(5));
-			}      
+				vimu2.clear();
+
+			}
+			running = (cv::waitKey(5) != 27);
 		}
 	}
-
-    return 0;
+	cout << "finished." << endl;
+	return 0;
 }
